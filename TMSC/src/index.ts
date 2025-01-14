320180488e364bbbf9c257c797239a7edee0c68b
C0dingForFun <gwibantulini8@gmail.com>
2025-01-14 15:08:12 +0200

    arrays & object literals

--- TMSC/src/index.ts
+++ TMSC/src/index.ts
@@ -24,5 +24,47 @@ let anotherThing: undefined;
 something = null;
 anotherThing = undefined;
 
-console.log('Hello World');
+console.log('Hello');
 
+// Arrays
+
+let names: string[] = ['Mario','Luigi', 'Peach'];
+let ages: number[] = [25, 28, 24];
+
+names.push('Bowser');
+ages.push(35);
+
+// type inference with arrays
+
+let fruits = ['apples', 'pears', 'bananas', 'mangoes'];
+
+fruits.push('Peaches');
+
+const f = fruits[3]
+
+let things = [1, true, 'Hello']
+
+const t = things[0]  // t can be any of the 3 data types
+
+// object litreals
+
+let user: { firstName: string, age: number, id: number} = {
+    firstName: 'Mario',
+    age: 25,
+    id: 1
+}
+
+user.firstName = 'Paach';
+user.id = 2
+
+// type inference with object literals
+
+let person = {
+    name: 'Luigi',
+    score: 35
+}
+
+person.name = 'Bowser';
+person.score = 40;
+
+const score = person.score;
