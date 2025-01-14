34553db50a73b6243d96ef14d11493393d80ef1a
C0dingForFun <gwibantulini8@gmail.com>
2025-01-14 14:36:42 +0200

    better workflow with tsconfig

---
+++ TMSC/dist/index.js
@@ -0,0 +1,19 @@
+// "use strict";
+// Type Basics
+let age = 30;
+let firstName = 'Mario';
+let isFictional;
+age = 31;
+firstName = 'Luigi';
+isFictional = true;
+let planet = 'Earth';
+let moons = 1;
+let isLarge = false;
+planet = 'Saturn';
+moons = 145;
+isLarge = true;
+// null & undefined
+let something;
+let anotherThing;
+something = null;
+anotherThing = undefined;
